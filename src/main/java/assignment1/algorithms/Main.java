e339e5596f113fdf5117f8459f7f44e2aa58b5ad
ragnarlotbrak <kasenov205@gmail.com>
2025-09-28 18:28:25 +0500

    ||Update|| Main!!!

--- src/main/java/assignment1/algorithms/Main.java
+++ src/main/java/assignment1/algorithms/Main.java
@@ -1,121 +1,100 @@
 package assignment1.algorithms;
 
-import assignment1.metrics.CSVWriter;
-import assignment1.util.Shuffle;
-import assignment1.util.Metrics;
+import assignment1.metrics.*;
+import assignment1.util.*;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        int[] base = {73, 15, 92, 48, 6, 57, 34, 88, 21, 100,
-                62, 3, 79, 46, 27, 9, 53, 64, 81, 18,
-                95, 40, 7, 30, 67, 12, 55, 85, 99, 24,
-                14, 37, 71, 2, 90, 44, 59, 26, 83, 11,
-                97, 32, 4, 68, 19, 61, 29, 75, 8, 50};
-        int n  = base.length;
 
+        String algo = "mergesort";
+        int size = 30;
+        String output = "result.csv";
+        int k = 5;
+
+        for(int i = 0; i < args.length; i++){
+            switch (args[i]){
+                case "--algo": algo = args[++i]; break;
+                case "--size": size = Integer.parseInt(args[++i]); break;
+                case "--output": output = args[++i]; break;
+                case "--k": k = Integer.parseInt(args[++i]); break;
+            }
+        }
 
         Metrics metrics = new Metrics();
 
+        try (CSVWriter writer = new CSVWriter(output)) {
+            switch(algo.toLowerCase()){
+                case "mergesort":
+                    int[] arr1 = new Random().ints(size, 0, 100).toArray();
+                    Shuffle.shuffle(arr1);
+                    MergeSort ms = new MergeSort(metrics);
+                    ms.mergeSort(arr1);
+                    System.out.println("Merge sort result: ");
+                    System.out.print(Arrays.toString(arr1));
+                    System.out.println("\nMetrics of MergeSort: " + metrics);
+                    writer.writeRecord("\nMergeSort", arr1.length,
+                            metrics.getComparisons(),
+                            metrics.getSwaps(),
+                            metrics.getMaxDepth());
+                    break;
+                case "quicksort":
+                    int[] arr2 = new Random().ints(size, 0, 100).toArray();
+                    Shuffle.shuffle(arr2);
+                    QuickSort qs = new QuickSort(metrics);
+                    qs.quickSort(arr2);
+                    System.out.println("QuickSort result: ");
+                    System.out.print(Arrays.toString(arr2));
+                    System.out.println("\nMetrics of QuickSort: " + metrics);
+                    writer.writeRecord("QuickSort", arr2.length,
+                            metrics.getComparisons(),
+                            metrics.getSwaps(),
+                            metrics.getMaxDepth());
+                    break;
+                case "select":
+                    int[] arr3 = new Random().ints(size, 0, 100).toArray();
+                    DeterministicSelect ds = new DeterministicSelect(metrics);
+                    System.out.println("Array: " + Arrays.toString(arr3));
+                    System.out.print("Enter the number of elements in the array: ");
+                    Scanner in = new Scanner(System.in);
+                    int n = in.nextInt() - 1;
+                    int nthElement = ds.deterministicSelect(arr3.clone(), n);
+                    System.out.println((k + 1) + "-th element is: " + nthElement);
+                    System.out.println("Metrics of Select: " + metrics);
+                    writer.writeRecord("Select", arr3.length,
+                            metrics.getComparisons(),
+                            metrics.getSwaps(),
+                            metrics.getMaxDepth());
+                    break;
 
-        try (CSVWriter writer = new CSVWriter("result.csv")) {
-            //--------------Merge Sort Part------------------
-            MergeSort ms = new MergeSort(metrics);
-
-            int[] arr1 = base.clone();
-            Shuffle.shuffle(arr1);
-            ms.mergeSort(arr1);
-
-            System.out.println("Merge sort result: ");
-            for (int i = 0; i < n; i++) {        //можно использовать просто System.out.print(Arrays.toString(arr*));!
-                System.out.print(arr1[i] + " "); //но хочу чтоб был цикл)
+                case "closest":
+                    ClosestPairOfPoints.Point[] points = {
+                            new ClosestPairOfPoints.Point(2, 3),
+                            new ClosestPairOfPoints.Point(12, 30),
+                            new ClosestPairOfPoints.Point(40, 50),
+                            new ClosestPairOfPoints.Point(5, 1),
+                            new ClosestPairOfPoints.Point(12, 10),
+                            new ClosestPairOfPoints.Point(3, 4)
+                    };
+                    ClosestPairOfPoints cp = new ClosestPairOfPoints(metrics);
+                    ClosestPairOfPoints.Result result = cp.closest(points);
+                    System.out.println("Closest Pair Of Points result: " + result.p1 + " and " + result.p2);
+                    System.out.println("Distance = " + result.dist);
+                    System.out.println("Metrics of ClosestPairOfPoints: " + metrics);
+                    System.out.println(" ");
+                    writer.writeRecord("ClosestPairOfPoints", points.length,
+                            metrics.getComparisons(),
+                            metrics.getSwaps(),
+                            metrics.getMaxDepth());
+                    break;
+                default:
+                    System.out.println("Unknown algorithm: " + algo);
             }
-            System.out.println("\nMetrics of Merge sort: " + metrics);
-
-            writer.writeRecord("\nMergeSort", arr1.length,
-                    metrics.getComparisons(),
-                    metrics.getSwaps(),
-                    metrics.getMaxDepth());
-
-            System.out.println(" ");
-
-            //--------------Quick Sort Part------------------
-            metrics.reset();
-            int[] arr2 = base.clone();
-            Shuffle.shuffle(arr2);
-
-            QuickSort qs = new QuickSort(metrics);
-            qs.quickSort(arr2);
-
-            System.out.println("\nQuick sort result: ");
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr2[i] + " ");
-            }
-            System.out.println("\nMetrics of Quick sort: " + metrics);
-
-            writer.writeRecord("QuickSort", arr2.length,
-                    metrics.getComparisons(),
-                    metrics.getSwaps(),
-                    metrics.getMaxDepth());
-
-            System.out.println(" ");
-
-
-            //--------------D Select Part------------------
-            int[] arr = {12, 67, 5, 43, 0, 28, 39, 70, 18, 54,
-                    7, 61, 25, 33, 46, 2, 68, 14, 37, 49};
-
-            metrics.reset();
-            DeterministicSelect ds = new DeterministicSelect(metrics);
-            System.out.println("\nDeterministic Select result:");
-            System.out.println("Array: " + Arrays.toString(arr));
-            System.out.print("      Enter the number of elements in the array: ");
-
-            Scanner in = new Scanner(System.in);
-            int k = in.nextInt() - 1;
-
-            int kthElement = ds.deterministicSelect(arr.clone(), k);
-            System.out.println((k + 1) + "-th element is: " + kthElement);
-            System.out.println("Metrics of Select: " + metrics);
-
-
-            writer.writeRecord("Select", arr.length,
-                    metrics.getComparisons(),
-                    metrics.getSwaps(),
-                    metrics.getMaxDepth());
-
-
-            //--------------CPOP Part------------------
-            metrics.reset();
-            ClosestPairOfPoints cp = new ClosestPairOfPoints(metrics);
-
-            ClosestPairOfPoints.Point[] points = {
-                    new ClosestPairOfPoints.Point(2, 3),
-                    new ClosestPairOfPoints.Point(12, 30),
-                    new ClosestPairOfPoints.Point(40, 50),
-                    new ClosestPairOfPoints.Point(5, 1),
-                    new ClosestPairOfPoints.Point(12, 10),
-                    new ClosestPairOfPoints.Point(3, 4)
-            };
-
-            ClosestPairOfPoints.Result result = cp.closest(points);
-            System.out.println(" ");
-
-            System.out.println("\nClosest pair result: " + result.p1 + " and " + result.p2);
-            System.out.println("Distance = " + result.dist);
-            System.out.println("Metrics of ClosestPairOfPoints: " + metrics);
-
-            writer.writeRecord("ClosestPairOfPoints", points.length,
-                    metrics.getComparisons(),
-                    metrics.getSwaps(),
-                    metrics.getMaxDepth());
         }
         catch (IOException e){
-            System.err.println("error in working of CSV: " + e.getMessage());
+            System.err.println("Error in working of CSV: " + e.getMessage());
         }
     }
 }
